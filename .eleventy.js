9537d0b6eb794c67b33fb91233daff4ace149657
Gungx214 <benzarnawal@yahoo.com>
2026-08-25 14:20:51 +0800

    Register displayDate filter for date formatting
    Added a filter to format dates in 'en-US' locale.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
 module.exports = function(eleventyConfig) {
+  // Register the missing displayDate filter
+  eleventyConfig.addFilter("displayDate", (dateObj) => {
+    return new Date(dateObj).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      timeZone: "UTC"
+    });
+  });
+
   // Copy static assets to the output folder
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy("src/images");
